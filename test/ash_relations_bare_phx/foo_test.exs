4215681ee50f9a7d3013b55652290a1e143145d0
Jarl André Hübenthal <jarlah@protonmail.com>
2024-01-09 11:23:10 +0100

    fix

--- test/ash_relations_bare_phx/foo_test.exs
+++ test/ash_relations_bare_phx/foo_test.exs
@@ -8,7 +8,7 @@ defmodule AshRelationsBarePhx.FooTest do
     test "has_many bars" do
       foo1 = Foo.FooResource.create!(%{title: "Foo 1"})
       bar1 = Bar.BarResource.create!(%{title: "Bar 1", foo_resource_id: foo1.id})
-      bar1 = Bar.BarResource.create!(%{title: "Bar 2", foo_resource_id: foo1.id})
+      bar2 = Bar.BarResource.create!(%{title: "Bar 2", foo_resource_id: foo1.id})
 
       assert %FooResource{bars: [bar1, bar2]} = AshRelationsBarePhx.Foo.load!(foo1, :bars)
     end
